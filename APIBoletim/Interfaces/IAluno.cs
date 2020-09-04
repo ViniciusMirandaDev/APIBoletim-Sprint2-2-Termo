c476273e910da4649f24a450dece6c62c7d791d6
FIC <FIC@info.local>
2020-09-04 00:31:51 -0300

    Projeto Finalizado

---
+++ APIBoletim/Interfaces/IAluno.cs
@@ -0,0 +1,17 @@
+﻿using APIBoletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Interfaces
+{
+    interface IAluno
+    {
+        List<Aluno> LerTodos();
+        Aluno BuscarPorId(int id);
+        Aluno Cadastrar(Aluno a);
+        Aluno Alterar(int id, Aluno a);
+        void Excluir(int id);
+    }
+}
